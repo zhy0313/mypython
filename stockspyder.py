d879607f076cf5ee0ca357e2aaef89c679b3228d
ab053658 <AB053658wangyanwu@bjbangye.com>
2017-03-28 08:45:09 +0800

    stockspyder

---
+++ stockspyder.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 24 21:16:04 2017
+
+@author: AB053658
+"""
+
+import requests
+import re 
+from bs4 import BeautifulSoup
+import traceback
+
+'''
+输入：网页url
+输出：网页，text
+'''
+def getHTMLText(url):
+    pass
+
+
+'''
+输入：html
+输出：stocklist
+'''
+def getStockList(html,stocklist):
+    pass
+'''
+输入：个股url，stocklist，filepath
+输出：打印文件
+'''
+def getStockinfo(stocklist,stockurl,filepath):
+    pass
+
+def main():
+   stocklist_url='http://quote.eastmoney.com/stocklist.html'
+   stockinfo_url='https://gupiao.baidu.com/stock/'
+   
+   pass
+
+main()
