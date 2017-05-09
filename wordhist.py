dbf3d6d1a651684807b8fe93775e1a9febc20143
ab053658 <AB053658wangyanwu@bjbangye.com>
2017-05-09 17:50:04 +0800

    myprocess_line

--- wordhist.py
+++ wordhist.py
@@ -6,6 +6,7 @@ Created on Wed May 03 21:19:18 2017
 """
 
 import string
+import re
 '''
 输入:filename
 输出：包含单词和单词出现次数的字典hist
@@ -17,6 +18,12 @@ def process_file(filename):
         process_line(line,hist)
     return hist
 
+def myprocess_file(filename):
+    hist=dict()
+    fp=open(filename)
+    for line in fp:
+        myprocess_line(line,hist)
+    return hist
 '''
 输入：string，和hist字典
 输出：把string 分割成一个个word，添加进hist，value为出现次数
@@ -29,7 +36,16 @@ def process_line(line,hist):
         word=word.lower()
         
         hist[word]=hist.get(word,0)+1
-            
+
+def myprocess_line(line,hist):
+    line=line.replace('-',' ')
+    
+    for word in line.split():
+        word=re.sub(r'[^A-Za-z0-9]',' ',word)
+        word=word.lower()
+        
+        hist[word]=hist.get(word,0)+1
+        
 def  total_words(hist):
     return sum(hist.values())
 
@@ -37,9 +53,15 @@ def different_words(hist):
     return len(hist)
 
 hist=process_file('emma.txt')
+hist1=myprocess_file('emma.txt')
 
 
 
-print(hist)
+#print(hist)
+for i in range(10):
+    print("  ")
+print(hist1)
 print(total_words(hist))
 print(different_words(hist))
+print(total_words(hist1))
+print(different_words(hist1))
